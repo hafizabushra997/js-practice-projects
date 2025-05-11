651dc69f815577a8cc0e9506c27fec2e8e927049
hafizabushra997 <hafizabushra997@gmail.com>
2025-05-11 16:43:05 +0500

    Build Click tracker app

---
+++ click-tracker/app.js
@@ -0,0 +1,14 @@
+let clickCount=document.getElementById('clickCount')
+let clickBtn=document.getElementById('clickBtn')
+let resetBtn=document.getElementById('resetBtn')
+let count=0
+
+clickBtn.addEventListener('click',()=>{
+    count++
+    clickCount.innerText=count
+})
+resetBtn.addEventListener('click',()=>{
+    count=0
+        clickCount.innerText=count
+    // clickCount.innerText = Math.max(count, 0);  // For later use
+})
